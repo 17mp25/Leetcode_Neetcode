94aa163a711c824dc610d0f0ab09a1e17cd8f6d2
Manoj <manojcpatil17@gmail.com>
2025-12-31 11:59:09 +0530

    Initial Commit + ContainsDuplicate class added with two different implementations

--- java-neetcode/src/main/java/arrays_and_hashing/ContainsDuplicate.java
+++ java-neetcode/src/main/java/arrays_and_hashing/ContainsDuplicate.java
@@ -1,6 +1,8 @@
 package arrays_and_hashing;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicate {
 
@@ -16,8 +18,19 @@ public class ContainsDuplicate {
         return false;
     }
 
+    public static boolean hasDuplicateWithExtraSpace(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums){
+            if(!set.add(num)){
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int [] arr = {1,2,3,1,5};
         System.out.println(hasDuplicateWithoutExtraSpace(arr));
+        System.out.println(hasDuplicateWithExtraSpace(arr));
     }
 }
