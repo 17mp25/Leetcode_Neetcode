5be79ff172cf24967854b6ec1f7b32547e080b9d
Manoj <manojcpatil17@gmail.com>
2025-12-31 12:38:27 +0530

    Initial Commit + IsAnagram class added with two different implementations

--- java-neetcode/src/main/java/arrays_and_hashing/IsAnagram.java
+++ java-neetcode/src/main/java/arrays_and_hashing/IsAnagram.java
@@ -17,9 +17,27 @@ public class IsAnagram {
         return countS.equals(countT);
     }
 
+    public static boolean isAnagramOptimised(String s, String t){
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int[] count = new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+        for(int c: count){
+            if(c!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String s = "anagram";
         String t = "nagaram";
         System.out.println(isAnagram(s, t)); // true
+        System.out.println(isAnagramOptimised(s, t)); // true
     }
 }
