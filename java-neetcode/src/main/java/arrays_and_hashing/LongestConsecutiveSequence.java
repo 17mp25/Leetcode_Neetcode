7cd4bcd257d4142db3331c72d40daa81c4405435
Manoj <manojcpatil17@gmail.com>
2026-02-09 18:51:47 +0530

    Tested using hashmap and linked hash map

--- java-neetcode/src/main/java/arrays_and_hashing/LongestConsecutiveSequence.java
+++ java-neetcode/src/main/java/arrays_and_hashing/LongestConsecutiveSequence.java
@@ -26,8 +26,32 @@ public class LongestConsecutiveSequence {
         }
         return longestStreak;
     }
+
+    public static int longConsecutiveCharInString(String s){
+        Set<Character> set = new HashSet<>();
+        for(char c :s.toCharArray()){
+            set.add(c);
+        }
+        int longestStreak = 0;
+        for(char c: set){
+            if(!set.contains((char)c-1)){
+                char currentChar = c;
+                int currentStreak = 1;
+
+                while(set.contains((char)(currentChar+1))){
+                    currentChar++;
+                    currentStreak++;
+                }
+                longestStreak = Math.max(currentStreak, longestStreak);
+            }
+
+        }
+        return longestStreak;
+    }
     public static void main(String[] args) {
         int[] nums = {1,3,4,5,2,7,100,101,105,104,103,102,106};
+        String s = "aabccdefg";
+        System.out.println(longConsecutiveCharInString(s)); // Output: 10 (for
         System.out.println(longestConsecutive(nums)); // Output: 4
 
     }
