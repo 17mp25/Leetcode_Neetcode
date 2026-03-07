7e8a9691108df04ef35d1de127b33b8ee00e5dd3
Manoj <manojcpatil17@gmail.com>
2026-03-07 23:18:22 +0530

    Added substring i/p

--- java-neetcode/src/main/java/practiceproblems/slidingwindow/LongestSubstring.java
+++ java-neetcode/src/main/java/practiceproblems/slidingwindow/LongestSubstring.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class LongestSubstring {
 
     public static int longestSustring(String s){
-        int left = 0;
+        int left = 1;
         int maxLength = 0;
         Set<Character> set = new HashSet<>();
         for(int right = 0; right < s.length(); right++){
@@ -19,7 +19,8 @@ public class LongestSubstring {
     }
 
     public static void main(String[] args) {
-        String s = "abcbcdefghijklkkjzkbcbb";
+        String s = "abcbcdefghijklkkjzk" +
+                "bcbb";
         System.out.println(longestSustring(s)); // Output: 3 (for "abc")
     }
 }
