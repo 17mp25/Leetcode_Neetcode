ef8a485f23cbf5912bbe26e36b89132c1c504479
Manoj <manojcpatil17@gmail.com>
2026-04-08 22:09:27 +0530

    TrappingWater Added

--- java-neetcode/src/main/java/arrays_and_hashing/TrappingWater.java
+++ java-neetcode/src/main/java/arrays_and_hashing/TrappingWater.java
@@ -32,7 +32,7 @@ public class TrappingWater {
     }
 
     public static void main(String[] args) {
-        int [] height = {0,1,0,9,2,1,0,1,3,2,2,1,2,1};
+        int [] height = {0,1,0,2,1,0,1,3,2,2,1,2,1};
         System.out.println(trap(height));
     }
 }
